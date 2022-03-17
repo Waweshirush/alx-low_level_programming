5d538ab706e511d02bcabc7024740393814eb76c
Waweshirush <brayowaweshirush@gmail.com>
2022-03-16 17:30:24 -0700

    My nineth task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ _putchar ((c % 10) + '0');
 }
 if (b < 9)
 {
-_putchar(',');
+_putchar (',');
 _putchar (' ');
 }
 }
@@ -36,10 +36,11 @@ _putchar ((c / 10) + '0');
 _puthar ((c % 10) + '0');
 if (b < 9)
 {
-_putchar(',');
+_putchar (',');
 _putchar (' ');
 }
 }
 }
 _putchar ('\n');
+}
 }
