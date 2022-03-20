2e9e274d0c6573b834b6d22b13e0d61c01437cda
Waweshirush <brayowaweshirush@gmail.com>
2022-03-19 17:07:43 -0700

    My fourth task commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet without q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q')
+			continue;
+		else if (alphabet =='e')
+			continue;
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
